5cd48bf32094ca18f08f173edc3a9afa5f507af5
nebuch <dogancihad@gmail.com>
2017-01-21 02:38:53 +0300

    oldu

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1484955477,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/staticwaterbody-sheet0.png",
+		"images/staticwatergradient-sheet0.png",
+		"images/staticwaterinstance-sheet0.png",
+		"images/canvas.png",
+		"images/platform-sheet0.png",
+		"images/player-sheet0.png",
+		"images/physicswaterinstance-sheet0.png",
+		"images/havuz-sheet0.png",
+		"images/clouds-sheet0.png",
+		"images/staticwater-sheet0.png",
+		"images/otherpool-sheet0.png",
+		"images/pool1-sheet0.png",
+		"images/restartbutton-sheet0.png",
+		"images/spritefont.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
